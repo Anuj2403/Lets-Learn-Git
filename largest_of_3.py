5178de174554d47b4c81ea2556397a0ae80caa24
Pramod Wankhade <pramodwankhade360@gmail.com>
2021-10-18 15:26:07 -0700

    added largest of three

---
+++ largest_of_3.py
@@ -0,0 +1,10 @@
+a=input('Enter first number :\n')
+b=input('Enter second number:\n')
+c=input('Enter third number:\n')
+
+if a>b and a>c:
+        print('first number is largest')
+elif b>a and b>c:
+       print('second number is largest')
+else:
+       print('Third number is largest')
